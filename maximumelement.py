f4ac301fb27b7aaac5543f2ded00058fad268f7f
kibeyefa <kibeyefa@gmail.com>
2020-12-19 19:47:48 +0100

    first commit

---
+++ maximumelement.py
@@ -0,0 +1,26 @@
+stack, queries = list(), ("1", "2", "3")
+itterations = int(input("Enter an integer: "))
+
+def is_valid_query(query, queries):
+   return query in queries
+
+print(stack)
+
+for i in range(itterations):
+   array = []
+   entry = input()
+   array = entry.split(" ")
+   print(array)
+   query = array[0] 
+   if is_valid_query(query, queries):
+      if query == "1":
+         stack.append(array[-1])
+      elif query == "2":
+         stack.pop()
+      elif query == "3":
+         duplicate = stack.copy()
+         duplicate.sort()
+         print(duplicate[0])
+         
+
+   
---
+++ sparsestrings.py
@@ -0,0 +1,27 @@
+strings, queries = list(), list()
+no_of_strings = int(input("Enter number of strings: "))
+no_of_queries = int(input("Enter number of queries: "))
+
+for i in range(no_of_strings):
+   string = input("Enter string: ").lower()
+   strings.append(string)
+
+for j in range(no_of_queries):
+   query = input("Enter query: ").lower()
+   queries.append(query)
+
+
+def matchStrings(strings, queries):
+   dictionary, array = dict(), list()
+   for string in strings:
+      for query in queries:
+         if query == string:
+            dictionary.setdefault(query, 0)
+            dictionary[query] =  dictionary[query] + 1
+
+   for value in dictionary.values():
+      array.append(value)
+
+   return array
+
+print(str(matchStrings(strings, queries)))
